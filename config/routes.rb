60d0172c77326737bf2e491412cbfd234e236156
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-24 08:24:47 -0700

    Test find item by all attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 	namespace :api, defaults: {format: :json} do
 		namespace :v1 do
+			namespace :items do
+				get '/find' => 'search#show'
+				get '/find_all' => 'search#index'
+			end
 			resources :items, only: [:index, :show]
 		end
 	end
--- spec/requests/item_request_spec.rb
+++ spec/requests/item_request_spec.rb
@@ -26,4 +26,46 @@ RSpec.describe "Item request API", type: :request do
 		expect(response).to be_success
 		verify_item_attributes(item, db_item)
 	end
+
+	it "finds first item by name" do
+		db_item = create(:item)
+
+		get "/api/v1/items/find?name=#{db_item.name}"
+
+		item = JSON.parse(response.body)
+		expect(response).to be_success
+		verify_item_attributes(item, db_item)
+	end
+
+	it "finds first item by description" do
+		db_item = create(:item)
+
+		get "/api/v1/items/find?description=#{db_item.description}"
+
+		item = JSON.parse(response.body)
+		expect(response).to be_success
+		verify_item_attributes(item, db_item)
+	end
+
+	it "finds first item by unit_price" do
+		db_item = create(:item)
+
+		get "/api/v1/items/find?unit_price=#{db_item.unit_price}"
+
+		item = JSON.parse(response.body)
+		expect(response).to be_success
+		verify_item_attributes(item, db_item)
+	end
+
+	it "finds all items by name" do
+		items = create_list(:item, 3)
+		db_item = items.first
+
+		get "/api/v1/items/find_all?name=#{db_item.name}" 
+
+		item = JSON.parse(response.body).first
+
+		expect(response).to be_success
+		verify_item_attributes(item, db_item)
+	end
 end 
