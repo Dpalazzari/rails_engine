cde1aec66f7e5ced91245eba3420018b3710712a
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-24 08:11:03 -0700

    Complete item show api test

--- spec/requests/item_request_spec.rb
+++ spec/requests/item_request_spec.rb
@@ -16,7 +16,14 @@ RSpec.describe "Item request API", type: :request do
 		verify_item_attributes(item, db_item)
 	end
 
-	xit "" do
+	it "returns a single item" do
+		db_item = create(:item)
 
+		get "/api/v1/items/#{db_item.id}"
+
+		item = JSON.parse(response.body)
+
+		expect(response).to be_success
+		verify_item_attributes(item, db_item)
 	end
 end 
