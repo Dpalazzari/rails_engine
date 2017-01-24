b0e4123513c3b934b45cfd281f643dd17e06f1c3
Mike Schutte <tmikeschutte@gmail.com>
2017-01-24 13:00:19 -0700

    Add passing finder tests for transactions

--- spec/requests/api/v1/transactions/request_spec.rb
+++ spec/requests/api/v1/transactions/request_spec.rb
@@ -17,4 +17,49 @@ RSpec.describe 'Transactions API requests', type: :request do
     verify_transaction_attributes(transaction, db_transaction)
   end
 
+  it 'returns a single transaction' do
+    db_transaction = create(:transaction)
+
+    get "/api/v1/transactions/#{db_transaction.id}"
+    expect(response).to be_success
+    transaction = parse_body
+
+    verify_transaction_attributes(transaction, db_transaction)
+  end
+
+  it 'can find a transaction by id' do
+    db_transaction = create(:transaction)
+
+    get "/api/v1/transactions/find?id=#{db_transaction.id}"
+    transaction = parse_body
+
+    verify_transaction_attributes(transaction, db_transaction)
+  end
+
+  it 'can find a transaction by credit card number' do
+    db_transaction = create(:transaction)
+
+    get "/api/v1/transactions/find?credit_card_number=#{db_transaction.credit_card_number}"
+    transaction = parse_body
+
+    verify_transaction_attributes(transaction, db_transaction)
+  end
+
+  it 'can find a transaction by result' do
+    db_transaction = create(:transaction)
+
+    get "/api/v1/transactions/find?result=#{db_transaction.result}"
+    transaction = parse_body
+
+    verify_transaction_attributes(transaction, db_transaction)
+  end
+
+  it 'can find a transaction by result ignoring case' do
+    db_transaction = create(:transaction)
+
+    get "/api/v1/transactions/find?result=#{db_transaction.result.upcase}"
+    transaction = parse_body
+
+    verify_transaction_attributes(transaction, db_transaction)
+  end
 end
