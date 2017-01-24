806e1fe79ab36c70a2e834f75f47267ae5419cb6
Mike Schutte <tmikeschutte@gmail.com>
2017-01-24 13:14:50 -0700

    Finish testing suite for transaction requests

--- spec/requests/api/v1/transactions/request_spec.rb
+++ spec/requests/api/v1/transactions/request_spec.rb
@@ -62,4 +62,95 @@ RSpec.describe 'Transactions API requests', type: :request do
 
     verify_transaction_attributes(transaction, db_transaction)
   end
+
+  it 'can find a transaction by created_at' do
+    db_transaction = create(:transaction)
+
+    get "/api/v1/transactions/find?created_at=#{db_transaction.created_at}"
+    transaction = parse_body
+
+    verify_transaction_attributes(transaction, db_transaction)
+  end
+
+  it 'can find a transaction by updated_at' do
+    db_transaction = create(:transaction)
+
+    get "/api/v1/transactions/find?updated_at=#{db_transaction.updated_at}"
+    transaction = parse_body
+
+    verify_transaction_attributes(transaction, db_transaction)
+  end
+
+  it 'can find all transactions by credit card number' do
+    number = create_list(:transaction, 2).first.credit_card_number
+    create(:transaction, credit_card_number: 1234)
+
+    get "/api/v1/transactions/find_all?credit_card_number=#{number}"
+    transactions = parse_body
+    numbers      = transactions.map { |transaction| transaction['credit_card_number'] }
+
+    expect(Transaction.count).to eq(3)
+    expect(transactions.count).to eq(2)
+    expect(numbers.all? { |n| n == number }).to be(true)
+  end
+
+  it 'can find all transactions by result' do
+    result = create_list(:transaction, 2).first.result
+    create(:transaction, result: 'nothing')
+
+    get "/api/v1/transactions/find_all?result=#{result}"
+    transactions = parse_body
+    results      = transactions.map { |transaction| transaction['result'] }
+
+    expect(Transaction.count).to eq(3)
+    expect(transactions.count).to eq(2)
+    expect(results.all? { |r| r == result }).to be(true)
+  end
+
+  it "finds all transactions matching created_at timestamps" do
+    create_list(:transaction, 3)
+    created = Transaction.first.created_at.to_json
+
+    get "/api/v1/transactions/find_all?created_at=#{created}"
+
+    expect(response).to be_success
+
+    transactions = JSON.parse(response.body)
+    created_ats = transactions.map { |transaction| transaction['created_at'] }
+
+    expect(transactions.count).to eq(3)
+    expect(created_ats.all? { |c| created.include?(c) }).to be true
+  end
+
+  it "finds all transactions matching updated_at timestamps" do
+    create_list(:transaction, 3)
+    updated = Transaction.first.updated_at.to_json
+
+    get "/api/v1/transactions/find_all?updated_at=#{updated}"
+
+    expect(response).to be_success
+
+    transactions = JSON.parse(response.body)
+    updated_ats = transactions.map { |transaction| transaction['updated_at'] }
+
+    expect(transactions.count).to eq(3)
+    expect(updated_ats.all? { |u| updated.include?(u) }).to be true
+  end
+
+  it 'shows a random transaction' do
+    create_list(:transaction, 100)
+
+    get '/api/v1/transactions/random'
+    transaction1   = parse_body
+    db_transaction = Transaction.find(transaction1['id'])
+
+    get '/api/v1/transactions/random'
+    transaction2 = parse_body
+    db_transaction2 = Transaction.find(transaction2['id'])
+
+    verify_transaction_attributes(transaction1, db_transaction)
+    verify_transaction_attributes(transaction2, db_transaction2)
+
+    expect(transaction1).to_not eq(transaction2)
+  end
 end
