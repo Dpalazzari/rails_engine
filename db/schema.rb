4c640ac7af11d674aa9cffc1262cca66846f8122
Mike Schutte <tmikeschutte@gmail.com>
2017-01-24 11:34:55 -0700

    Migrate customers

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,13 @@ ActiveRecord::Schema.define(version: 20170124175733) do
   enable_extension "plpgsql"
   enable_extension "citext"
 
+  create_table "customers", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "items", force: :cascade do |t|
     t.citext   "name"
     t.citext   "description"
