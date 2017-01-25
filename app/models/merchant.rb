31432db50b806bdb83be97718541191e6fcd76ce
Mike Schutte <tmikeschutte@gmail.com>
2017-01-25 11:11:39 -0700

    Add formatting for revenue methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,6 @@
 class Merchant < ApplicationRecord
+  include ActionView::Helpers::NumberHelper
+
   has_many :items
   has_many :invoices
   has_many :invoice_items, through: :invoices
@@ -12,15 +14,22 @@ class Merchant < ApplicationRecord
   def revenue(date = nil)
     return revenue_by_date(date) if date
 
-    successful_transactions
+    cents = successful_transactions
       .joins(invoice: [:invoice_items])
       .sum('invoice_items.quantity * invoice_items.unit_price')
+    revenue_dollars(cents)
   end
 
   def revenue_by_date(date)
-    successful_transactions
+    cents = successful_transactions
       .where('invoices.created_at = ?', date)
       .joins(invoice: [:invoice_items])
       .sum('invoice_items.quantity * invoice_items.unit_price')
+    revenue_dollars(cents)
+  end
+
+  def revenue_dollars(cents)
+    revenue = number_with_precision(cents / 100.00, precision: 2).to_s
+    { 'revenue' => revenue }
   end
 end
