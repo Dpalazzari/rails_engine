65f695cf091cc46fbc2922f361f621fd8d185802
Mike Schutte <tmikeschutte@gmail.com>
2017-01-25 15:42:47 -0700

    Adjust invoice item unit price formatting
    Changes to match spec harness expectations

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -11,6 +11,8 @@ class Api::V1::InvoiceItems::SearchController < ApplicationController
 	private
 
 	def invoice_item_params
+		params[:unit_price] = (params[:unit_price].to_f * 100.00).to_s if params[:unit_price] &&
+			params[:unit_price].include?('.')
 		params.permit(:id, :invoice_id, :item_id, :quantity, :unit_price, :created_at, :updated_at)
 	end
 
