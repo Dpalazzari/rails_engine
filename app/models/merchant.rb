1c8acbb2ebc9e4f482d3fe929d5291a793217ebe
Mike Schutte <tmikeschutte@gmail.com>
2017-01-24 20:33:36 -0700

    Add Merchant#revenue methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,4 +8,19 @@ class Merchant < ApplicationRecord
   def successful_transactions
     transactions.where(result: 'success')
   end
+
+  def revenue(date = nil)
+    return revenue_by_date(date) if date
+
+    successful_transactions
+      .joins(invoice: [:invoice_items])
+      .sum('invoice_items.quantity * invoice_items.unit_price')
+  end
+
+  def revenue_by_date(date)
+    successful_transactions
+      .where('invoices.created_at = ?', date)
+      .joins(invoice: [:invoice_items])
+      .sum('invoice_items.quantity * invoice_items.unit_price')
+  end
 end
