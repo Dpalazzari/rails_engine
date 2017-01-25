42bf3fc9be3ca7b379e163814c68aef85b200445
Mike Schutte <tmikeschutte@gmail.com>
2017-01-24 20:38:12 -0700

    Add model tests for revenue instance methods

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -21,5 +21,53 @@ RSpec.describe Merchant, type: :model do
         expect(merchant.successful_transactions).to match_array([successful_transaction])
       end
     end
+
+    describe '#revenue' do
+      it 'returns total revenue' do
+        merchant = create(:merchant)
+        merchant.invoices << create_list(:invoice, 2)
+        merchant.invoices.each do |invoice|
+          invoice.transactions << create(:transaction)
+          invoice.invoice_items << create(:invoice_item, unit_price: 100)
+          invoice.invoice_items << create(:invoice_item, unit_price: 150, quantity: 2)
+        end
+
+        expect(merchant.revenue).to eq(800)
+      end
+    end
+
+    describe '#revenue_by_date(date)' do
+      it 'returns total revenue' do
+        merchant = create(:merchant)
+        merchant.invoices << create_list(:invoice, 2)
+        merchant.invoices.each do |invoice|
+          invoice.transactions << create(:transaction)
+          invoice.invoice_items << create(:invoice_item, unit_price: 100)
+          invoice.invoice_items << create(:invoice_item, unit_price: 150, quantity: 2)
+        end
+        new_date = merchant.invoices.first.created_at + 10.days
+        merchant.invoices.first.update(created_at: new_date)
+        merchant.reload
+
+        expect(merchant.revenue_by_date(new_date)).to eq(400)
+      end
+    end
+
+    describe '#revenue(date)' do
+      it 'returns total revenue' do
+        merchant = create(:merchant)
+        merchant.invoices << create_list(:invoice, 2)
+        merchant.invoices.each do |invoice|
+          invoice.transactions << create(:transaction)
+          invoice.invoice_items << create(:invoice_item, unit_price: 100)
+          invoice.invoice_items << create(:invoice_item, unit_price: 150, quantity: 2)
+        end
+        new_date = merchant.invoices.first.created_at + 10.days
+        merchant.invoices.first.update(created_at: new_date)
+        merchant.reload
+
+        expect(merchant.revenue(new_date)).to eq(400)
+      end
+    end
   end
 end
