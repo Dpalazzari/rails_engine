b5a5f6a285e41138b38dfc89ba1b206b0655f803
Mike Schutte <tmikeschutte@gmail.com>
2017-01-25 05:37:29 -0700

    Add merchant_id to item strong params for search

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::Items::SearchController < ApplicationController
 	private
 
 	def item_params
-		params.permit(:id, :name, :description, :unit_price, :created_at, :updated_at)
+		params.permit(:id, :name, :description, :unit_price, :created_at, :updated_at, :merchant_id)
 	end
 
 end
