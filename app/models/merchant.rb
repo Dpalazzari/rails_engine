78a4a81580e41536e294d1a20ff46344c0b8884e
Mike Schutte <tmikeschutte@gmail.com>
2017-01-24 19:10:02 -0700

    Add Merchant#successful_transactions

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,8 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+
+  def successful_transactions
+    transactions.where(result: 'success')
+  end
 end
