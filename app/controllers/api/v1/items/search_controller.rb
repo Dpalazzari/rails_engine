1e49b4d6882797122efc98a6f9bf4c2078f76058
Mike Schutte <tmikeschutte@gmail.com>
2017-01-25 05:53:07 -0700

    Add price formatter for item search controller

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -11,6 +11,7 @@ class Api::V1::Items::SearchController < ApplicationController
 	private
 
 	def item_params
+		params[:unit_price] = (params[:unit_price].to_f * 100.00).round if params[:unit_price]
 		params.permit(:id, :name, :description, :unit_price, :created_at, :updated_at, :merchant_id)
 	end
 
