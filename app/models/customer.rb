74593285bdd269a11ab506a1debe3f1755d2575e
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-26 07:21:02 -0700

    Add test for customer favorite merchant

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,10 @@ class Customer < ApplicationRecord
   has_many :invoices
   has_many :merchants, through: :invoices
   has_many :transactions, through: :invoices
+
+  def favorite_merchant
+  	# returns a merchant where the customer 
+	  # has conducted the most successful transactions
+	  binding.pry
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,27 @@ RSpec.describe Customer, type: :model do
     it { should have_many :merchants }
     it { should have_many :transactions }
   end
+
+  context 'business analytics' do
+
+  	describe '#favorite_merchant' do
+  		it 'returns the favorite merchant' do
+  			customer 			 = create(:customer)
+  			top_merchant 	 = create(:merchant)
+  			worse_merchant = create(:merchant)
+  			invoice 			 = create(:invoice, merchant: top_merchant, customer: customer)
+  			invoice_2			 = create(:invoice, merchant: worse_merchant, customer: customer)
+  			transactions 	 = create_list(:transaction, 100, result: 'success', invoice: invoice)
+  			top_merchant.invoices 	<< invoice
+  			worse_merchant.invoices << invoice_2
+  			# binding.pry
+  			# top_merchant.invoices 	<< create_list(:invoice, 100, customer: customer)
+  			# worse_merchant.invoices << create_list(:invoice, 5, customer: customer)
+
+  			# returns a merchant where the customer 
+	  		# has conducted the most successful transactions
+	  		expect(customer.favorite_merchant).to eq(top_merchant)
+  		end
+  	end
+  end
 end
