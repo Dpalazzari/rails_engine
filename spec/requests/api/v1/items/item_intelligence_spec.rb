ebccc00390359641662e9d4818ca5b530a4644ce
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-26 14:19:29 -0700

    Refactor item spec test

--- spec/requests/api/v1/items/item_intelligence_spec.rb
+++ spec/requests/api/v1/items/item_intelligence_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe 'Item Best Day Endpoint API' do
 		it 'returns the best day by date' do
 			item 		= create(:item)
 			invoice = create(:invoice)
+			invoice_2 = create(:invoice)
+      date = invoice_2.created_at
+      invoice_2.update(created_at: date - 10.days) 
 			create_list(:invoice_item, 4, item: item, invoice: invoice)
 
 			get "/api/v1/items/#{item.id}/best_day"
 
 			time = JSON.parse(response.body)
-			# binding.pry
 
 			expect(response).to be_success
 			expect(invoice.created_at.to_json).to include(time['best_day'])
