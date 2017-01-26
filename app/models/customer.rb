3bec3c1cb6a32037a8fb1ad001bdbed7410b3d00
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-26 07:36:17 -0700

    add some sql logic

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,5 +7,13 @@ class Customer < ApplicationRecord
   	# returns a merchant where the customer 
 	  # has conducted the most successful transactions
 	  binding.pry
+	  rails_engine_development=# select count('invoices.merchant_id') as merchant_count from customers
+join invoices
+on invoices.customer_id = customers.id
+join transactions
+on transactions.invoice_id = invoices.id
+join merchants
+on invoices.merchant_id = merchants.id
+rails_engine_development-# where transactions.result = 'success';
   end
 end
