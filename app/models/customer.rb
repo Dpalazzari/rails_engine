9f4c9eacd4c5a10a74d3c30c51b94a38bb680994
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-26 08:06:50 -0700

    Complete active record code for sql query

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,16 +6,23 @@ class Customer < ApplicationRecord
   def favorite_merchant
   	# returns a merchant where the customer 
 	  # has conducted the most successful transactions
-	  binding.pry
-	  select count('invoices.merchant_id') as merchant_count, merchants.*
-		from customers
-		join invoices
-		on invoices.customer_id = customers.id
-		join transactions
-		on invoices.id = transactions.invoice_id
-		join merchants
-		on invoices.merchant_id = merchants.id where transactions.result = 'success' and customers.id = 66
-		group by merchants.id order by merchant_count DESC
-limit(1);
+
+	 #  select count('invoices.merchant_id') as merchant_count, merchants.*
+		# from customers
+		# join invoices
+		# on invoices.customer_id = customers.id
+		# join transactions
+		# on invoices.id = transactions.invoice_id
+		# join merchants
+		# on invoices.merchant_id = merchants.id where transactions.result = 'success' and customers.id = 66
+		# group by merchants.id order by merchant_count DESC
+		# limit(1);
+		# binding.pry
+		merchants
+		.select('merchants.*, count(invoices.merchant_id) as merchant_count')
+		.joins(invoices: [:transactions])
+		.merge(Transaction.successful).group('merchants.id')
+		.order('merchant_count desc')
+		.first
   end
 end
