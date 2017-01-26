43333d8b6b7c780bd61908a1356f85d643142509
Mike Schutte <tmikeschutte@gmail.com>
2017-01-26 05:22:27 -0700

    Extract successful transaction logic
    Move from Merchant to Transaction and refactor Merchant#revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,18 +10,6 @@ RSpec.describe Merchant, type: :model do
   end
 
   context 'business analytics' do
-    describe '#successful_transactions' do
-      it 'returns all successful transactions' do
-        merchant = create(:merchant)
-        successful_invoice = create(:invoice, merchant: merchant)
-        failed_invoice = create(:invoice, merchant: merchant)
-        successful_transaction = create(:transaction, result: 'success', invoice: successful_invoice)
-        failed_transaction = create(:transaction, result: 'failed', invoice: failed_invoice)
-
-        expect(merchant.successful_transactions).to match_array([successful_transaction])
-      end
-    end
-
     describe '#revenue' do
       it 'returns total revenue' do
         merchant = create(:merchant)
