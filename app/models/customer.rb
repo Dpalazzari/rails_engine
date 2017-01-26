290d58ae2eba539c280668edcf3e8d15fbf1aabf
Drew Palazzari <drewpalazzari@hotmail.com>
2017-01-26 08:45:19 -0700

    Pass customer favorite merchant intel test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,24 +4,11 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
 
   def favorite_merchant
-  	# returns a merchant where the customer 
-	  # has conducted the most successful transactions
-
-	 #  select count('invoices.merchant_id') as merchant_count, merchants.*
-		# from customers
-		# join invoices
-		# on invoices.customer_id = customers.id
-		# join transactions
-		# on invoices.id = transactions.invoice_id
-		# join merchants
-		# on invoices.merchant_id = merchants.id where transactions.result = 'success' and customers.id = 66
-		# group by merchants.id order by merchant_count DESC
-		# limit(1);
-		# binding.pry
-		merchants
+	  merchants
 		.select('merchants.*, count(invoices.merchant_id) as merchant_count')
 		.joins(invoices: [:transactions])
-		.merge(Transaction.successful).group('merchants.id')
+		.merge(Transaction.successful)
+		.group('merchants.id')
 		.order('merchant_count desc')
 		.first
   end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -12,19 +12,15 @@ RSpec.describe Customer, type: :model do
   	describe '#favorite_merchant' do
   		it 'returns the favorite merchant' do
   			customer 			 = create(:customer)
-  			top_merchant 	 = create(:merchant)
-  			worse_merchant = create(:merchant)
-  			invoice 			 = create(:invoice, merchant: top_merchant, customer: customer)
-  			invoice_2			 = create(:invoice, merchant: worse_merchant, customer: customer)
-  			transactions 	 = create_list(:transaction, 100, result: 'success', invoice: invoice)
-  			top_merchant.invoices 	<< invoice
-  			worse_merchant.invoices << invoice_2
-  			# binding.pry
-  			# top_merchant.invoices 	<< create_list(:invoice, 100, customer: customer)
-  			# worse_merchant.invoices << create_list(:invoice, 5, customer: customer)
-
-  			# returns a merchant where the customer 
-	  		# has conducted the most successful transactions
+				top_merchant 	 = create(:merchant)
+				worse_merchant = create(:merchant)
+				invoice 			 = create(:invoice, merchant: top_merchant, customer: customer)
+				invoice_3 		 = create(:invoice, merchant: top_merchant, customer: customer)
+				invoice_2			 = create(:invoice, merchant: worse_merchant, customer: customer)
+				create(:transaction, result: 'success', invoice: invoice)
+				create(:transaction, result: 'success', invoice: invoice_2)
+				create(:transaction, result: 'success', invoice: invoice_3)
+  			
 	  		expect(customer.favorite_merchant).to eq(top_merchant)
   		end
   	end
