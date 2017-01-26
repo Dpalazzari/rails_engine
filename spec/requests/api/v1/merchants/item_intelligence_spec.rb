8d835b19001a6ff39bfcf8748001b0a922500c45
Mike Schutte <tmikeschutte@gmail.com>
2017-01-26 06:17:45 -0700

    Refactor item intellgence specs with before block

--- spec/requests/api/v1/merchants/item_intelligence_spec.rb
+++ spec/requests/api/v1/merchants/item_intelligence_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Item intelligence endpoints' do
   describe 'GET /api/v1/merchants/most_items?quantity=x' do
-    it 'returns top 5 merchants with most items sold' do
-      merchants = create_list(:merchant, 10)
+    before do
+      merchants = create_list(:merchant, 5)
       (merchants.length - 1).times do |i|
         invoices = create_list(:invoice, i + 1, merchant: merchants[i])
         invoices.each do |invoice|
@@ -11,14 +11,25 @@ RSpec.describe 'Item intelligence endpoints' do
           create_list(:invoice_item, i + 1, invoice: invoice)
         end
       end
+    end
 
+    it 'returns top 3 merchants with most items sold' do
       get '/api/v1/merchants/most_items?quantity=5'
-      json_merchants = JSON.parse(response.body)
 
       expect(response).to be_success
 
-      result   = Merchant.with_most_items(5).map { |m| m.invoice_items.sum(:quantity) }
-      expected = [81, 64, 49, 36, 25]
+      result   = Merchant.with_most_items(3).map { |m| m.invoice_items.sum(:quantity) }
+      expected = [16, 9, 4]
+      expect(result).to eq(expected)
+    end
+
+    it 'returns top 4 merchants with most items sold' do
+      get '/api/v1/merchants/most_items?quantity=5'
+
+      expect(response).to be_success
+
+      result   = Merchant.with_most_items(4).map { |m| m.invoice_items.sum(:quantity) }
+      expected = [16, 9, 4, 1]
       expect(result).to eq(expected)
     end
   end
