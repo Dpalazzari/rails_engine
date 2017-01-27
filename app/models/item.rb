aab99a5b02602eb136ff0a01d6f0727fb5d61402
Mike Schutte <tmikeschutte@gmail.com>
2017-01-26 18:52:27 -0700

    Refactor AR queries

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,7 @@ class Item < ApplicationRecord
   default_scope { order(:id) }
 
   def best_day
-		invoices
-      .joins(:invoice_items)
+		invoices.joins(:invoice_items)
       .group('invoices.id')
       .order("sum(invoice_items.quantity) DESC, invoices.created_at DESC")
       .first
@@ -16,20 +15,11 @@ class Item < ApplicationRecord
   end
 
   def self.most_revenue(quantity)
-    Item.find_by_sql(
-      "select i.*, sum(in_it.quantity * in_it.unit_price) as revenue
-      from items i
-      join invoice_items in_it
-      on i.id = in_it.item_id
-      join invoices inv
-      on in_it.invoice_id = inv.id
-      join transactions t
-      on t.invoice_id = inv.id
-      where t.result = 'success'
-      group by i.id
-      order by revenue desc
-      limit (#{quantity})"
-     )
+    unscoped.joins(invoice_items: [invoice: :transactions])
+      .merge(Transaction.successful)
+      .group('items.id')
+      .order('sum(invoice_items.quantity * invoice_items.unit_price) desc')
+      .limit(quantity)
   end
 
   def self.most_items(quantity)
